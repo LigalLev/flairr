80febacef2b7f1a4d6af1c7c115f36dd810295e0
LigalLev <ligalelias@gmail.com>
2023-05-31 23:03:37 +0300

    additional files

--- src/cmps/app-footer.jsx
+++ src/cmps/app-footer.jsx
@@ -7,53 +7,14 @@ import { removeFromCart, checkout } from '../store/gig.actions'
 import { UserMsg } from './user-msg.jsx'
 
 export function AppFooter() {
-    const [isCartShown, setIsCartShown] = useState(false)
-    const cart = useSelector(storeState => storeState.gigModule.cart)
-    const count = useSelector(storeState => storeState.userModule.count)
-    const cartTotal = cart.reduce((acc, gig) => acc + gig.price, 0)
-
-    async function onCheckout() {
-        try {
-            const score = await checkout(cartTotal)
-            showSuccessMsg(`Charged, your new score: ${score.toLocaleString()}`)
-        } catch(err) {
-            showErrorMsg('Cannot checkout')
-        }
-    }
+  
 
     return (
         <footer className="app-footer">
             <p>
-                coffeerights - count: {count}
+                coffeerights 
             </p>
-            {cart.length > 0 &&
-                <h5>
-                    <span>{cart.length}</span> Products in your Cart
-                    <button className="btn-link" onClick={(ev) => {
-                        ev.preventDefault();
-                        setIsCartShown(!isCartShown)
-                    }}>
-                        ({(isCartShown) ? 'hide' : 'show'})
-                    </button>
-                </h5>
-            }
-
-            {isCartShown && cart.length > 0 && <section className="cart" >
-                <h5>Your Cart</h5>
-                <ul>
-                    {
-                        cart.map((gig, idx) => <li key={idx}>
-                            <button onClick={() => {
-                                removeFromCart(gig._id)
-                            }}>x</button>
-                            {gig.vendor}
-                        </li>)
-                    }
-                </ul>
-                <p>Total: ${cartTotal.toLocaleString()} </p>
-                <button onClick={onCheckout}>Checkout</button>
-            </section>}
-            <UserMsg />
+            
         </footer>
     )
 }
---
+++ src/cmps/gig-preview.jsx
@@ -0,0 +1,3 @@
+export function GigPreview(){
+    return <div>lol</div>
+}
