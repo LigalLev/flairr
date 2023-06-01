9420d4812a12f03a4dd3fa5f2f3d66b7c0401545
Karin Zohar <karinzohar3@gmail.com>
2023-06-01 15:12:35 +0300

    fixed footer and started hero

---
+++ src/cmps/hero.jsx
@@ -0,0 +1,18 @@
+export function Hero() {
+
+    const items = [
+        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606996/flairr/hero/hero1_dq2qdv.png" />,
+        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero2_bcsmlh.jpg" />,
+        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero5_eiz8iq.jpg" />,
+        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero4_nn8gjb.jpg" />,
+        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero6_ozknih.jpg" />,
+        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606992/flairr/hero/hero3_atgfmp.jpg" />
+      ]
+
+    return (
+        <section className="hero main-layout full">
+            <h1>hero</h1>
+
+        </section>
+    )
+}
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Hero } from '../cmps/hero'
 
 export function HomePage() {
     const dispatch = useDispatch()
     const count = useSelector(storeState => storeState.userModule.count)
 
     return (
-        <section>
-
-            
-        </section >
+        <section className="home-page">
+            <Hero />
+        </section>
     )
 }
