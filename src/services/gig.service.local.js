6319c38238c35f787d4958d040a779dcce087053
Adi Ben Ami <adi.afriat@gmail.com>
2023-06-01 13:30:55 +0300

    added some demo data for gigs

--- src/services/gig.service.local.js
+++ src/services/gig.service.local.js
@@ -191,7 +191,115 @@ function getEmptyGig() {
                   "Minimalist",
                 ],
                 likedByUsers: ['mini-user'] 
-                }
+                },
+                {
+                  _id: "i106",
+                  title: "I will design your I will design your landscape, backyard, garden, patio and hardscape",
+                  price: 100,
+                  owner: {
+                      "_id": "u106",
+                      "fullname": "Dazzlee",
+                      "imgUrl": "https://res.cloudinary.com/dm4cdho4d/image/upload/v1685611471/flairr/kari_iomkbb.jpg",
+                      "level": "basic",
+                      "rate": 5
+                  },
+                  daysToMake: 7,
+                  description: "As a professional landscape architect and urban designer with a Master's degree and over 650 completed projects on Fiverr, I am fully committed to providing exceptional backyard design services to my clients. Whether you are looking to create a tranquil garden, a stylish patio, or a unique hardscape feature, I have the skills and expertise to bring your vision to life. My services include 2D and 3D design, high-quality renderings, and comprehensive project management. I strive to deliver projects on time, to the highest standards, with quick delivery and unlimited revisions to ensure your complete satisfaction. If you are ready to transform your backyard into an oasis, please do not hesitate to contact me to discuss your project and view my portfolio of recent work. I am confident that my expertise and attention to detail will help you to create the backyard of your dreams.",
+                  imgUrl: ["https://res.cloudinary.com/dm4cdho4d/image/upload/v1685607683/flairr/design-and-render-interior-and-exterior-projects-within-24h_zsqyzv.jpg","https://res.cloudinary.com/dm4cdho4d/image/upload/v1685607668/flairr/design-and-render-interior-and-exterior-projects-within-24h_auxzm5.jpg", "https://res.cloudinary.com/dm4cdho4d/image/upload/v1685607656/flairr/design-and-render-interior-and-exterior-projects-within-24h_uovqdk.jpg", "https://res.cloudinary.com/dm4cdho4d/image/upload/v1685607618/flairr/design-and-render-interior-and-exterior-projects-within-24h_buoa7g.jpg" ],
+                  tags: [
+                      "designing",
+                      "garden",
+                      "architecture",
+                      "landscape"
+                  ],
+                  likedByUsers: ['mini-user']
+              }, 
+              {
+                  _id: "i107",
+                  title: "I will design your garden, backyard and render landscape design",
+                  price: 50,
+                  owner: {
+                      "_id": "u107",
+                      "fullname": "Eshan",
+                      "imgUrl": "https://res.cloudinary.com/dm4cdho4d/image/upload/v1685611433/flairr/1/3f9e6cba-00c6-4c33-bde0-a43bd48a93cf_huyzfh.jpg",
+                      "level": "basic",
+                      "rate": 4.9
+                  },
+                  daysToMake: 2,
+                  description: "My name is Eshan. I'm a professional landscape architect. For over 5 years, I've been doing landscape design with professional landscape architectural practice. With this Gig I am ready to create your landscape designs, such as gardens, backyard , front yard, pool, patio, outdoor spaces, and indoor spaces",
+                  imgUrl: ["https://res.cloudinary.com/dm4cdho4d/image/upload/v1685608039/flairr/1/design-your-dream-garden_aoqh4g.jpg","https://res.cloudinary.com/dm4cdho4d/image/upload/v1685608021/flairr/1/design-your-dream-garden_nn6s82.jpg", "https://res.cloudinary.com/dm4cdho4d/image/upload/v1685607975/flairr/1/design-your-dream-garden_p4y9kc.jpg","https://res.cloudinary.com/dm4cdho4d/image/upload/v1685608001/flairr/1/design-your-dream-garden_gbyher.jpg"  ],
+                  tags: [
+                      "front yard",
+                      "garden",
+                      "3d design",
+                      "landscape design"
+                  ],
+                  likedByUsers: ['mini-user']
+              },
+              {
+                  _id: "i108",
+                  title: "I will draw perspectives, conceptual sketches, and landscape drawings",
+                  price: 20,
+                  owner: {
+                      "_id": "u108",
+                      "fullname": "Ramodha",
+                      "imgUrl": "https://res.cloudinary.com/dm4cdho4d/image/upload/v1685612017/flairr/2/8e9b3518-9a37-4ce9-b02d-685182462d1c_mmiti6.jpg",
+                      "level": "basic",
+                      "rate": 4.9
+                  },
+                  daysToMake: 3,
+                  description: "Hi, I'm Ramodha, a passionate graphic designer with 4 years of experience in Landscape Architecture. I do Freelancer, graphic design, and landscape sketching in my free time. Drop a message and I'll get to .you as soon as I can",
+                  imgUrl: ["https://res.cloudinary.com/dm4cdho4d/image/upload/v1685608279/flairr/2/draw-perspectives-conceptual-sketches-and-landscape-drawings-49be_qlnld8.png","https://res.cloudinary.com/dm4cdho4d/image/upload/v1685608227/flairr/2/draw-perspectives-conceptual-sketches-and-landscape-drawings-49be_mhcgfj.jpg", "https://res.cloudinary.com/dm4cdho4d/image/upload/v1685608204/flairr/2/draw-perspectives-conceptual-sketches-and-landscape-drawings-49be_jgrrwn.jpg","https://res.cloudinary.com/dm4cdho4d/image/upload/v1685608250/flairr/2/draw-perspectives-conceptual-sketches-and-landscape-drawings-49be_qbmtoh.jpg"  ],
+                  tags: [
+                      "perspective",
+                      "pencil sketch",
+                      "landscape design"
+                  ],
+                  likedByUsers: ['mini-user']
+              }, 
+              {
+                  _id: "i109",
+                  title: "I will design landscape, backyard, patio, garden, and frontyard",
+                  price: 70,
+                  owner: {
+                      "_id": "u109",
+                      "fullname": "Yona",
+                      "imgUrl": "https://res.cloudinary.com/dm4cdho4d/image/upload/v1685612830/flairr/3/97f53003-65fc-417f-a194-0e8f48cc4770_hfhnpi.jpg",
+                      "level": "basic",
+                      "rate": 5
+                  },
+                  daysToMake: 10,
+                  description: "I am an architect, with professional skills in creative Designs. I have the skill set to create a great design and visualizations using the latest software. There are added qualities to the designs because of the different perspective applied rather that just the design itself",
+                  imgUrl: ["https://res.cloudinary.com/dm4cdho4d/image/upload/v1685612810/flairr/3/do-graphic-design-logo-designs-specifically_y8j8my.jpg","https://res.cloudinary.com/dm4cdho4d/image/upload/v1685612790/flairr/3/do-graphic-design-logo-designs-specifically_f7gvav.jpg", "https://res.cloudinary.com/dm4cdho4d/image/upload/v1685612776/flairr/3/do-graphic-design-logo-designs-specifically_ozoyct.jpg","https://res.cloudinary.com/dm4cdho4d/image/upload/v1685612757/flairr/3/do-graphic-design-logo-designs-specifically_s7bcv8.jpg"  ],
+                  tags: [
+                      "landscape",
+                      "pool design",
+                      "landscape design"
+                  ],
+                  likedByUsers: ['mini-user']
+              },
+              {
+                  _id: "i110",
+                  title: "I will create your front garden backyard patio landscape design",
+                  price: 80,
+                  owner: {
+                      "_id": "u110",
+                      "fullname": "Maheshika",
+                      "imgUrl": "https://res.cloudinary.com/dm4cdho4d/image/upload/v1685613877/flairr/4/874e937a-1369-4199-993d-f17530bd9872_t4j4uw.jpg",
+                      "level": "basic",
+                      "rate": 4.9
+                  },
+                  daysToMake: 3,
+                  description: "I am a Professional Landscape Architect with almost 05 years of experience in landscape/yard/garden design. I'll be able to design your landscape with a unique style according to your requirements, such as minimalistic gardens, dessert landscapes, midcentury modern, bohemian style, you name it! Doesn't matter if you don't have a clue, will try my best to bring your dream yard to reality.",
+                  imgUrl: ["https://res.cloudinary.com/dm4cdho4d/image/upload/v1685614006/flairr/4/create-your-garden-backyard-patio-landscape-design_hurv6a.jpg","https://res.cloudinary.com/dm4cdho4d/image/upload/v1685613988/flairr/4/create-your-garden-backyard-patio-landscape-design_ahpprc.jpg", "https://res.cloudinary.com/dm4cdho4d/image/upload/v1685613969/flairr/4/create-your-garden-backyard-patio-landscape-design_kgg59h.jpg","https://res.cloudinary.com/dm4cdho4d/image/upload/v1685613945/flairr/4/create-your-garden-backyard-patio-landscape-design_n6ttbe.jpg" ],
+                  tags: [
+                      "perspective",
+                      "pencil sketch",
+                      "landscape design"
+                  ],
+                  likedByUsers: ['mini-user']
+              }
+
         ]
         utilService.saveToStorage(STORAGE_KEY, gigs)
 
