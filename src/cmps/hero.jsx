7148d68bd17ef93a98eb605531045a1a14a030d9
Karin Zohar <karinzohar3@gmail.com>
2023-06-01 18:15:30 +0300

    added data to hero

--- src/cmps/hero.jsx
+++ src/cmps/hero.jsx
@@ -1,17 +1,54 @@
 export function Hero() {
-
     const items = [
-        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606996/flairr/hero/hero1_dq2qdv.png" />,
-        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero2_bcsmlh.jpg" />,
-        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero5_eiz8iq.jpg" />,
-        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero4_nn8gjb.jpg" />,
-        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero6_ozknih.jpg" />,
-        <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606992/flairr/hero/hero3_atgfmp.jpg" />
-      ]
+        {
+            imgUrl: "https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606996/flairr/hero/hero1_dq2qdv.png",
+            name: 'Valentina',
+            Rating: '',
+            occupation: 'AI Artist'
+        },
+        {
+            imgUrl: "https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero2_bcsmlh.jpg",
+            name: 'Andrea',
+            Rating: '',
+            occupation: 'Fashion Designer'
+        },
+        {
+            imgUrl: "https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606992/flairr/hero/hero3_atgfmp.jpg",
+            name: 'Moon',
+            Rating: '',
+            occupation: 'Marketing Expert'
+        },
+        {
+            imgUrl: "https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero4_nn8gjb.jpg",
+            name: 'Ritika',
+            Rating: '',
+            occupation: 'Shoemaker and Designer'
+        },
+        {
+            imgUrl: "https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero5_eiz8iq.jpg",
+            name: 'Zach',
+            Rating: '',
+            occupation: 'Bar Owner'
+        },
+        {
+            imgUrl: "https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero6_ozknih.jpg",
+            name: 'Gabrielle',
+            Rating: '',
+            occupation: 'Video Editor'
+        },
+
+        // <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero2_bcsmlh.jpg" />,
+        // <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero5_eiz8iq.jpg" />,
+        // <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero4_nn8gjb.jpg" />,
+        // <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero6_ozknih.jpg" />,
+        // <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606992/flairr/hero/hero3_atgfmp.jpg" />
+    ]
 
     return (
         <section className="hero main-layout full">
-            <h1>hero</h1>
+            <div className="full">
+                <img src="https://res.cloudinary.com/dqhfnvtca/image/upload/v1685606993/flairr/hero/hero6_ozknih.jpg" alt="" />
+            </div>
 
         </section>
     )
