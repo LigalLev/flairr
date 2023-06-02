3258f09555627d461c8ea2674eeb00630a051b01
Karin Zohar <karinzohar3@gmail.com>
2023-06-02 20:03:48 +0300

    hero is fully designed

--- src/cmps/hero.jsx
+++ src/cmps/hero.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Link } from "react-router-dom"
 export function Hero() {
     const slides = [
         {
@@ -40,6 +41,8 @@ export function Hero() {
 
 
     ]
+
+    const popularTags = ['Website Design', 'wordPress', 'Logo Design', 'AI services']
 
     const [currSlideIdx, setCurrSlideIdx] = useState(0)
 
@@ -85,18 +88,39 @@ export function Hero() {
                 </section>
             ))}
 
-            <section className='filtering-area'>
+
+            <section className="filtering-area" >
 
                 <h1>
                     Find the right <span>freelance service, </span>right away
                 </h1>
-                
-                <input 
-                type="text"
-                placeholder='Search for any device...'
-                />
 
-            </section>
+                <div className='search-bar'>
+                    <input type="text" placeholder='Search for any device...' name="search-text" id="hero-search-text" />
+
+                    <button className='search-btn'>
+                        <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentFill"><path d="m15.89 14.653-3.793-3.794a.37.37 0 0 0-.266-.109h-.412A6.499 6.499 0 0 0 6.5 0C2.91 0 0 2.91 0 6.5a6.499 6.499 0 0 0 10.75 4.919v.412c0 .1.04.194.11.266l3.793 3.794a.375.375 0 0 0 .531 0l.707-.707a.375.375 0 0 0 0-.53ZM6.5 11.5c-2.763 0-5-2.238-5-5 0-2.763 2.237-5 5-5 2.762 0 5 2.237 5 5 0 2.762-2.238 5-5 5Z" /></svg>
+                    </button>
+
+                </div>
+
+                <div className='popular-tags'>
+                    <p>Popular:</p>
+                    <ul>
+                        {popularTags.map(tag =>
+                            <li>
+                                <button>
+                                    <Link to="/gig">
+                                        {tag}
+                                    </Link>
+                                </button>
+                            </li>
+                        )}
+
+                    </ul>
+                </div>
+
+            </section >
 
         </section >
     )
