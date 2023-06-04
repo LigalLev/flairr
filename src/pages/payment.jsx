ae973a0c160ece817dfa700e8510021d5371d7fc
Adi Ben Ami <adi.afriat@gmail.com>
2023-06-04 23:39:31 +0300

    added payment cmp

--- src/pages/payment.jsx
+++ src/pages/payment.jsx
@@ -1,9 +1,16 @@
 import { useParams} from "react-router-dom"
+import {CCPayment} from '../cmps/cc-payment'
 
 
 export function Payment () {
 
     return <div>
+        <CCPayment/>
+        
+
+
+
+
         <button>Pay</button>
     </div>
 }
