695c47e6d18fcaa8e1c8881d862a26f3b8fbd2be
Karin Zohar <karinzohar3@gmail.com>
2023-06-07 01:12:47 +0300

    add gig form is complete and working, not yet designed

--- src/pages/gig-edit.jsx
+++ src/pages/gig-edit.jsx
@@ -12,7 +12,14 @@ export function GigEdit() {
     const navigate = useNavigate()
     const params = useParams()
     const gigToEdit = useSelector((storeState) => storeState.gigModule.gigs.find(gig => gig._id === params.gigId)) || gigService.getEmptyGig()
-    
+    const packageTypes = ['basic', 'standard', 'premium']
+    const categories = ['Graphics & Design', 'Digital Marketing', 'Writing & Translation', 'Video & Animation', 'Music & Audio', 'Programming & Tech', 'Photography', 'Business', 'AI Services']
+    const tags = [
+        "logo-design",
+        "artisitic",
+        "proffesional",
+        "accessible"
+    ]
     // const [updatedGig, setUpdatedGig] = useState(gigToEdit)
 
     // useEffect(() => {
@@ -71,11 +78,11 @@ export function GigEdit() {
         <section className="gig-edit">
             <h1>Add a new gig</h1>
             <Formik
-                initialValues={{...gigToEdit}}
+                initialValues={{ ...gigToEdit }}
                 validationSchema={gigSchema}
                 onSubmit={onSubmit}
             >
-                {({ errors, touched, dirty, setFieldValue}) => (
+                {({ errors, touched, dirty, setFieldValue }) => (
                     <Form className='formik'>
                         <label htmlFor="title">Gig Title</label>
                         <Field as={CustomInput} name="title" placeholder="I will..." />
@@ -85,15 +92,43 @@ export function GigEdit() {
                         <Field as="textarea" name="description" placeholder="Add a description of your gig." />
                         {errors.description && touched.description && <div>{errors.description}</div>}
 
-                        <label htmlFor="price">Price</label>
-                        <Field as={CustomInput} name="packages.basic.price" />
-                        {getIn(errors, 'packages.basic.price') && getIn(touched, 'packages.basic.price') && <div>{getIn(errors, 'packages.basic.price')}</div>}
+                        <label htmlFor="category">Category</label>
+                        <Field name="category" as="select">
+                            {
+                                categories.map(category =>
+                                    <option key={category} value={category}>{category}</option>
+                                )
+                            }
+                        </Field>
 
-                        <label htmlFor="daysToMake">Days to make</label>
-                        <Field as={CustomInput} name="packages.basic.daysToMake" />
-                        {getIn(errors, 'packages.basic.daysToMake') && getIn(touched, 'packages.basic.daysToMake') && <div>{getIn(errors, 'packages.basic.daysToMake')}</div>}
+                        <div className="tags-select">
+                            <h6>Tags</h6>
+                            {tags.map(tag =>
+                                <label key={tag}>
+                                    <Field type="checkbox" name="tags" value={tag} />
+                                    {tag}
+                                </label>
 
-                        <ImgUpload maxFiles={5} formikField={'imgUrls'} setFieldValue={setFieldValue}/>
+                            )}
+                        </div>
+
+                        {packageTypes.map(packageType =>
+                            <article key={packageType}>
+
+                                <h6>{packageType}</h6>
+                                <label htmlFor="price">Price</label>
+                                <Field as={CustomInput} name={`packages.${packageType}.price`} />
+                                {getIn(errors, `packages.${packageType}.price`) && getIn(touched, `packages.${packageType}.price`) && <div>{getIn(errors, `packages.${packageType}.price`)}</div>}
+
+                                <label htmlFor="daysToMake">Days to make</label>
+                                <Field as={CustomInput} name={`packages.${packageType}.daysToMake`} />
+                                {getIn(errors, `packages.${packageType}.daysToMake`) && getIn(touched, `packages.${packageType}.daysToMake`) && <div>{getIn(errors, `packages.${packageType}.daysToMake`)}</div>}
+                            </article>
+                        )}
+
+
+
+                        <ImgUpload maxFiles={5} formikField={'imgUrls'} setFieldValue={setFieldValue} />
                         <button type="submit">Create Gig</button>
 
                     </Form>
