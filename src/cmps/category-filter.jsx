c125f58169bf787aef30a89539848d8d0f56d17b
LigalLev <ligalelias@gmail.com>
2023-06-08 13:57:54 +0300

    filter bar design

--- src/cmps/category-filter.jsx
+++ src/cmps/category-filter.jsx
@@ -10,31 +10,6 @@ export function CategoryFilter(props) {
 
     const filterBy = useSelector((storeState) => storeState.gigModule.filterBy)
     const navigate = useNavigate()
-    // const debouncedFilterBySearch = utilService.debounce(onFilterBySearch, 500)
-    // const categories = ['Graphics & Design', 'Digital Marketing', 'Writing & Translation', 'Video & Animation', 'Music & Audio', 'Programming & Tech', ' Business', 'AI Services']
-
-    // useEffect(() => {
-    //     elInputRef.current.focus()
-    // }, [])
-
-    // useEffect(() => {
-    //     console.log('filterBy:', filterBy)
-
-    //     loadGigs(filterBy)
-    // }, [filterBy])
-
-    // function onFilterBySearch({ target }) {
-    //     console.log('target:', target.value)
-    //     const field = target.name
-    //     const value = target.value
-    //     const newFilterBy = { ...filterBy }
-    //     switch (field) {
-    //         case 'txt':
-    //             newFilterBy[field] = value
-    //             break
-    //     }
-    //     setFilterBy(newFilterBy)
-    // }
 
     function onClick(category) {
         // ev.preventDefault()
@@ -45,27 +20,13 @@ export function CategoryFilter(props) {
     }
 
 
-    // function onChange({ target }) {
-    //     console.log('ev:', target.value)
-    //     setFilterBySearch(target.value)
 
-    // }
-
-    // function handleKeyDown(ev) {
-    //     if (ev.key === 'Enter') {
-    //         onClick()
-    //     }
-    // }
-
-    return <ul className='category-btns'>
+    return <div className='categoty-btns-container main-layout full'> <ul className='category-btns'>
         {categories.map((category) => {
             return <li><button onClick={() => onClick(category)}>{category}</button></li>
         }
 
         )}
     </ul>
-
-
-    // included.map((includedItem) => {
-    //     return <li><span><svg width="
+    </div>
 }
