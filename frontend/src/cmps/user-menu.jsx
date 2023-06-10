0778bcf7c55c6bc2cedfa16cb8d7b41b236ababa
Karin Zohar <karinzohar3@gmail.com>
2023-06-10 17:15:47 +0300

    fixed user menu issue

--- frontend/src/cmps/user-menu.jsx
+++ frontend/src/cmps/user-menu.jsx
@@ -3,8 +3,8 @@ export function UserMenu({onLogout, user}) {
 
     return (
         <div className="user-menu">
-            <Link to={`user/${user._id}`} >Profile</Link>
-            <Link to="/gigs-dashboard">Become A Seller</Link>
+            <Link to={`user/${user._id}`} className='user-menu-link' >Profile</Link>
+            <Link to="/gigs-dashboard" className='user-menu-link' >Become A Seller</Link>
             <button onClick={onLogout}>Logout</button>
         </div>
     )
