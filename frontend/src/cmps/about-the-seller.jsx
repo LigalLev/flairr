28c787ea96800129c2172bf58bef166cd30facc9
Adi Ben Ami <adi.afriat@gmail.com>
2023-06-10 16:34:46 +0300

    added about the seller design

--- frontend/src/cmps/about-the-seller.jsx
+++ frontend/src/cmps/about-the-seller.jsx
@@ -21,23 +21,26 @@ export function AboutTheSeller({ gig }) {
                             <p className="seller-profession">{gig.owner.profession}</p>
                             <div className="rating-wrapper">
                                 <StarRating rating={gig.owner.rate} />
-                                <p><span>{gig.owner.rate}</span> ({gig.likedByUsers})</p>     
+                                <p><span>{gig.owner.rate}</span> ({gig.likedByUsers})</p>
                             </div>
                         </div>
                         <button className="contect-seller-btn">Contact me</button>
                     </div>
                 </div>
                 <div className="seller-personal-info-wrapper">
-                    <div>From
-                        <span>{gig.owner.from}</span>
+                    <div className="seller-short-info-wrapper">
+                        <div>From
+                            <span>{gig.owner.from}</span>
+                        </div>
+                        <div>Member since
+                            <span>{gig.owner.memberSince}</span>
+                        </div>
+                        <div>Languages
+                            <span>{gig.owner.Languages}</span>
+                        </div>
                     </div>
-                    <div>Member since
-                        <span>{gig.owner.memberSince}</span>
-                    </div>
-                    <div>Languages
-                        <span>{gig.owner.Languages}</span>
-                    </div>
-                    <div>
+
+                    <div className="seller-about">
                         <p>{gig.owner.about}</p>
                     </div>
                 </div>
