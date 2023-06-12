73f526db1b64e66af13bec74af535b272fd3ea82
LigalLev <ligalelias@gmail.com>
2023-06-12 15:26:39 +0300

    fixed status

--- frontend/src/cmps/seller-order-list.jsx
+++ frontend/src/cmps/seller-order-list.jsx
@@ -10,7 +10,7 @@ export function SellerOrderList({ orders, user, updateOrder}) {
                         <div className="seller-order flex column"> <span>Buyer</span>{order.buyer.fullname}</div>
                         <div className="seller-order flex column">  <span>Price</span>${order.gig.price}</div>
                         <div className="seller-order flex column status-options" >  <span>Status</span>
-                            {/* <StatusDropdown initialStatus={order.status}  */}
+                            <StatusDropdown initialStatus={order.status} 
                             onSelectStatus={(selectedStatus) => {
                                 // console.log(`will send http request with ${selectedStatus}`)
                                 const newOrder = {...order, status: selectedStatus}
