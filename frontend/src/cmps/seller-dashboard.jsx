851c621d350b835affcef9ee09f706923f8debbe
Adi Ben Ami <adi.afriat@gmail.com>
2023-06-12 12:22:30 +0300

    adding seller dashboard

--- frontend/src/cmps/seller-dashboard.jsx
+++ frontend/src/cmps/seller-dashboard.jsx
@@ -1,10 +1,34 @@
+import React from "react";
+import { render } from "react-dom";
+import {
+    CircularProgressbar,
+    CircularProgressbarWithChildren,
+    buildStyles
+} from "react-circular-progressbar"
+import "react-circular-progressbar/dist/styles.css"
+// import RadialSeparators from "./RadialSeparators";
 
-export function SellerDashboard({orders}){
+export function SellerDashboard({ orders }) {
+    
+
+    const percentage = 66;
 
 
-    return(
+    return (
         <section className="seller-dashboard">
             <h1>hey from dashboard</h1>
+            <div>
+                total income
+            </div>
+            <div lable="Default">
+                <CircularProgressbar value={percentage} text={`${percentage}%`} />
+            </div>
+            <div>
+                <CircularProgressbar value={percentage} text={`${percentage}%`} />
+            </div>
+            <div>
+                <CircularProgressbar value={percentage} text={`${percentage}%`} />
+            </div>
 
         </section>
     )
