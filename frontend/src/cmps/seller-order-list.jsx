11de56cf7665dc8d2ad7d6ecbb05fffc300d5311
Adi Ben Ami <adi.afriat@gmail.com>
2023-06-13 23:23:54 +0300

    chnged style in user details and responsiveness

--- frontend/src/cmps/seller-order-list.jsx
+++ frontend/src/cmps/seller-order-list.jsx
@@ -1,9 +1,9 @@
-import {StatusDropdown} from "./status-dropdown"
+import { StatusDropdown } from "./status-dropdown"
 import { loadOrders } from "../store/order.action"
-import {socketService} from "../services/socket.service";
+import { socketService } from "../services/socket.service";
 
 
-export function SellerOrderList({orders, user, updateOrder}) {
+export function SellerOrderList({ orders, user, updateOrder }) {
     return (
         <section className="seller-order-list-container">
             <ul className="seller-order-list">
@@ -11,21 +11,24 @@ export function SellerOrderList({orders, user, updateOrder}) {
                     return order.seller._id === user._id
                 }).map(order =>
                     <li key={order._id}>
-                        <div className="img-seller-order"><img src={order.gig.imgUrl} alt=""/></div>
+                        <div className="img-seller-order"><img src={order.gig.imgUrl} alt="" /></div>
                         {/* <div className="seller-order order-title flex column"><span>Gig</span>{order.gig.title}</div> */}
                         <div className="seller-order flex column buyer"><span>Buyer</span><p>{order.buyer.fullname}</p></div>
                         <div className="seller-order flex column"><span>Price</span>${order.gig.price}</div>
                         <div className="seller-order flex column status-options"><span>Status</span>
-                            <StatusDropdown initialStatus={order.status}
-                            onSelectStatus={(selectedStatus) => {
-                                                const newOrder = {...order, status: selectedStatus}
-                                                updateOrder(newOrder)
-                                                loadOrders({ sellerId: order.seller._id })
-                                                socketService.emit('update-order-status', {
-                                                    order: order,
-                                                    status: selectedStatus
-                                                })
-                                            }}/>
+                            <div className="statusdropdown-wrapper">
+                                <StatusDropdown initialStatus={order.status}
+                                    onSelectStatus={(selectedStatus) => {
+                                        const newOrder = { ...order, status: selectedStatus }
+                                        updateOrder(newOrder)
+                                        loadOrders({ sellerId: order.seller._id })
+                                        socketService.emit('update-order-status', {
+                                            order: order,
+                                            status: selectedStatus
+                                        })
+                                    }} />
+
+                            </div>
                         </div>
                     </li>)}
             </ul>
