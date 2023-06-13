6723b574e78a2eddca5b5d02587394453c24f652
Karin Zohar <karinzohar3@gmail.com>
2023-06-14 00:06:41 +0300

    improved user profile design

--- frontend/src/pages/user-details.jsx
+++ frontend/src/pages/user-details.jsx
@@ -71,13 +71,19 @@ export function UserDetails() {
               <p className='light'>@{user.username}</p>
             </div>
 
-            <div>
+            <div className='from-member-since'>
               <div className="from flex space-between">
-                <p>From</p>
-                <p className='bold'>{user.from}</p>
+                <div className='flex row user-from'>
+                  <svg width="12" height="16" viewBox="0 0 12 16" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path d="M5.38338 15.6772C0.842813 9.09472 0 8.41916 0 6C0 2.68628 2.68628 0 6 0C9.31372 0 12 2.68628 12 6C12 8.41916 11.1572 9.09472 6.61662 15.6772C6.31866 16.1076 5.68131 16.1076 5.38338 15.6772ZM6 8.5C7.38072 8.5 8.5 7.38072 8.5 6C8.5 4.61928 7.38072 3.5 6 3.5C4.61928 3.5 3.5 4.61928 3.5 6C3.5 7.38072 4.61928 8.5 6 8.5Z" /></g><defs><clipPath id="clip0"><rect width="12" height="16" /></clipPath></defs></svg>
+                  <p>From</p>
+                </div>
+                <p className='bold '>{user.from}</p>
               </div>
-              <div className="member-since flex space-between">
-                <p>Member since</p>
+              <div className="flex space-between">
+                <div className="flex row member-since" >
+                  <svg width="14" height="16" viewBox="0 0 14 16" xmlns="http://www.w3.org/2000/svg"><path d="M7 8C9.20938 8 11 6.20937 11 4C11 1.79063 9.20938 0 7 0C4.79063 0 3 1.79063 3 4C3 6.20937 4.79063 8 7 8ZM9.8 9H9.27812C8.58437 9.31875 7.8125 9.5 7 9.5C6.1875 9.5 5.41875 9.31875 4.72188 9H4.2C1.88125 9 0 10.8813 0 13.2V14.5C0 15.3281 0.671875 16 1.5 16H12.5C13.3281 16 14 15.3281 14 14.5V13.2C14 10.8813 12.1187 9 9.8 9Z" /></svg>
+                  <p>Member since</p>
+                </div>
                 <p className='bold'>{user.memberSince}</p>
               </div>
             </div>
@@ -103,7 +109,7 @@ export function UserDetails() {
 
 
 
-              <SellerOrderList orders={orders} user={user} updateOrder={updateOrder} /> 
+              <SellerOrderList orders={orders} user={user} updateOrder={updateOrder} />
             </div>
             :
             <div className="not-seller-action flex column">
